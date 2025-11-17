59e22692c76d2cb61e30b8cbaecd3c004d52e279
Vaibhav Diwan <divaibhav@gmail.com>
2025-11-17 15:53:19 +0530

    Defined BuildLinkedListHelper.java, with static method build, which returns SinglyLinkedList with some elements

---
+++ src/BuildLinkedListHelper.java
@@ -0,0 +1,13 @@
+public class BuildLinkedListHelper {
+    public static SinglyLinkedList build(){
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.insert(1);
+        singlyLinkedList.insert(56);
+        singlyLinkedList.insert(2);
+        singlyLinkedList.insert(15);
+        singlyLinkedList.insert(45);
+        singlyLinkedList.insert(8);
+        singlyLinkedList.insert(26);
+        return singlyLinkedList;
+    }
+}
