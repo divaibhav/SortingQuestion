8b28c18d5e6435e329bb7c56966266b328ff7c72
Vaibhav Diwan <divaibhav@gmail.com>
2025-11-17 15:55:14 +0530

    Defined MyMain.java with main method to get SinglyLinkedList by calling BuildLinkedListHelper.build() method and printed the linked list by calling traverse() method

---
+++ src/MyMain.java
@@ -0,0 +1,14 @@
+public class MyMain {
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = BuildLinkedListHelper.build();
+
+        singlyLinkedList.traverse();
+
+        //call the sort()
+        //print the sorted linked list using traverse() of SinglyLinkedList
+
+    }
+    public static void sort(SinglyLinkedList linkedList){
+        //complete the method to sort the given linked list
+    }
+}
